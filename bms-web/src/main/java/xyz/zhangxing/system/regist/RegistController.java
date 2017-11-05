5b35e64dd8f1f4387af282682c6a8a5fd868cddc
wanglei <997277250@qq.com>
2017-11-05 21:59:19 +0800

    bb

---
+++ bms-web/src/main/java/xyz/zhangxing/system/regist/RegistController.java
@@ -0,0 +1,14 @@
+package xyz.zhangxing.system.regist;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/system/regist")
+public class RegistController {
+
+    @RequestMapping("/registUI.htm")
+    public String registUI(){
+        return "/system/regist/regist.html";
+    }
+}
